6b892a9b22f563d31e6aa229d3fc4399c84da852
bobtwinkles <srkoser+github@gmail.com>
2018-10-12 12:55:48 -0400

    Add linux build

--- VST_MIDI/JuceLibraryCode/AppConfig.h
+++ VST_MIDI/JuceLibraryCode/AppConfig.h
@@ -36,11 +36,11 @@
 // BEGIN SECTION A
 
 #ifndef JUCE_DISPLAY_SPLASH_SCREEN
- #define JUCE_DISPLAY_SPLASH_SCREEN 1
+ #define JUCE_DISPLAY_SPLASH_SCREEN 0
 #endif
 
 #ifndef JUCE_REPORT_APP_USAGE
- #define JUCE_REPORT_APP_USAGE 1
+ #define JUCE_REPORT_APP_USAGE 0
 #endif
 
 // END SECTION A
@@ -269,7 +269,7 @@
 // juce_gui_extra flags:
 
 #ifndef    JUCE_WEB_BROWSER
- //#define JUCE_WEB_BROWSER 1
+ #define   JUCE_WEB_BROWSER 0
 #endif
 
 #ifndef    JUCE_ENABLE_LIVE_CONSTANT_EDITOR
